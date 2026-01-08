4802640228c0acc382775cd3b473b27dcd57aba8
Drew Fowler <dfowler@aurora.tech>
2026-01-08 11:26:37 -0700

    change zellij script

--- zellij/zellij-actions.sh
+++ zellij/zellij-actions.sh
@@ -2,10 +2,9 @@
 
 ZELLIJ_LAYOUTS="${ZELLIJ_LAYOUTS:-$(dirname "$0")/layouts}"
 
-#  zellij action new-tab --layout zellij/ai_with_editor_layout.kdl --name ai_agent_0
-zellij_new_ai_tab() {
+new-ai-tab() {
     zellij action new-tab -n "ai_agent_$1" \
         --layout "$ZELLIJ_LAYOUTS/ai_with_editor.kdl"
 }
 
-
+$@
